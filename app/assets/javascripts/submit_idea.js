133ebf8fa848f9fdda161dd10bb0b512bc085d83
HoffsMH <hecker.matthew@gmail.com>
2015-12-01 08:15:09 -0700

    added adding a new idea

---
+++ app/assets/javascripts/submit_idea.js
@@ -0,0 +1,21 @@
+function submit_idea() {
+  var title = $("#idea_title").val();
+  var body = $("#idea_body").val();
+  var tags = $("#idea_tags").val();
+  $.ajax({
+    method: "post",
+    data: {
+      idea: {
+        title: title,
+        body: body,
+        tags: tags
+      }
+    }
+  }).then(function (data){
+    clear_fields();
+    refresh_ideas();
+  }).fail(function (data) {
+    var error_text = JSON.parse(data.responseText).errors
+    $("#errors-row").html(error_text)
+  })
+}
