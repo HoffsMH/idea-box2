ecdf4b5eb0978a355b7eef43979a8e6f6dc0de7c
HoffsMH <hecker.matthew@gmail.com>
2015-12-02 14:17:32 -0700

    added feature testing for downgrading an idea

--- spec/features/user_can_downgrade_an_ideas_spec.rb
+++ spec/features/user_can_downgrade_an_ideas_spec.rb
@@ -1,43 +1,43 @@
 require 'rails_helper'
 
-RSpec.feature "UserCanUpgradeAnIdeas", type: :feature do
+RSpec.feature "UserCanDowngradeAnIdeas", type: :feature do
   before(:each) do
     idea1 = Idea.create(title: "this is a test title", body: "this is a test body", tags: {names: "hi there"})
   end
 
   let!(:idea2) do
-    Idea.create(title: "this is a test title2", body: "this is a test body2", tags: {names: "hi there2"})
+    Idea.create(title: "this is a test title2", body: "this is a test body2", quality: "genius", tags: {names: "hi there2"})
   end
 
-  it "allows the user to upgrade an idea quality", js: true do
+  it "allows the user to downgrade an idea quality", js: true do
     visit "/"
 
     within(:css, "div.row.idea#idea-2") do
-      expect(page).to have_content("swill")
+      expect(page).not_to have_content("swill")
       expect(page).not_to have_content("plausible")
-      expect(page).not_to have_content("genius")
+      expect(page).to have_content("genius")
 
       #first upgrade
-      page.find('.rate-btn-up').click
+      page.find('.rate-btn-down').click
 
       expect(page).not_to have_content("swill")
       expect(page).to have_content("plausible")
       expect(page).not_to have_content("genius")
 
       #second upgrade
-      page.find('.rate-btn-up').click
+      page.find('.rate-btn-down').click
 
-      expect(page).not_to have_content("swill")
+      expect(page).to have_content("swill")
       expect(page).not_to have_content("plausible")
-      expect(page).to have_content("genius")
+      expect(page).not_to have_content("genius")
 
       #third upgrade
-      page.find('.rate-btn-up').click
+      page.find('.rate-btn-down').click
 
       #doesn't go back to swill
-      expect(page).not_to have_content("swill")
+      expect(page).to have_content("swil")
       expect(page).not_to have_content("plausible")
-      expect(page).to have_content("genius")
+      expect(page).not_to have_content("genius")
     end
 
   end
