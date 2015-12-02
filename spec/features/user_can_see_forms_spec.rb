cbb1f8ace937434262b8efecede0e1e0e80194f4
HoffsMH <hecker.matthew@gmail.com>
2015-12-01 22:42:02 -0700

    added test for new idea form

--- spec/features/user_can_see_forms_spec.rb
+++ spec/features/user_can_see_forms_spec.rb
@@ -5,11 +5,14 @@ RSpec.feature "UserCanSeeForms", type: :feature do
     idea1 = Idea.create(title: "this is a test title", body: "this is a test body", tags: {names: "hi there"})
     idea2 = Idea.create(title: "this is a test title2", body: "this is a test body2", tags: {names: "hi there2"})
   end
-  it "can see all teh featurez", js: true do
+  it "it displays a form to make a new idea", js: true do
     visit "/"
     expect(page).to have_content("Title")
-    binding.pry
 
+    expect(page).to  have_selector("a[id=submit-idea-btn]")
+    expect(page).to  have_selector("input[id=idea_tags]")
+    expect(page).to  have_selector("textarea[id=idea_body]")
+    expect(page).to  have_selector("input[id=idea_title]")
     expect(page).to have_content("this is a test title2")
   end
 end
